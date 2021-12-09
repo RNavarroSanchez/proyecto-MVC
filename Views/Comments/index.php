3b48ad12a661dd941c1368d5886721e64a9571c8
Roberto Navarro Sanchez <robertonavarrosanchez@gmail.com>
2021-12-09 16:51:52 +0100

    poniendo webroot

--- Views/Comments/index.php
+++ Views/Comments/index.php
@@ -2,7 +2,7 @@
 <div class="row col-md-12 centered">
     <table class="table table-striped custab">
         <thead>
-        <a href="/proyecto-MVC/comments/create/" class="btn btn-primary btn-xs pull-right"><b>+</b> Añadir Comentario</a>
+        <a href=" <?php echo WEBROOT . 'comments/create' ?>" class="btn btn-primary btn-xs pull-right"><b>+</b> Añadir Comentario</a>
         <tr>
             <th>ID</th>
             <th>Comentario</th>
@@ -17,7 +17,7 @@
             echo "<td>" . $comment['id'] . "</td>";
             echo "<td>" . $comment['body'] . "</td>";
            
-            echo "<td class='text-center'><a class='btn btn-info btn-xs' href='/proyecto-MVC/comments/edit/" . $comment["id"] . "' ><span class='glyphicon glyphicon-edit'></span> Editar</a> <a href='/proyecto-MVC/comments/delete/" . $comment["id"] . "' class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove'></span> Borrar</a></td>";
+            echo "<td class='text-center'><a class='btn btn-info btn-xs' href=" . WEBROOT . 'comments/edit/'.$comment["id"] . " ><span class='glyphicon glyphicon-edit'></span> Editar</a> <a href=" . WEBROOT . 'comments/delete/'.$comment["id"] . " class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove'></span> Borrar</a></td>";
             echo "</tr>";
         }
         ?>
--- Views/Posts/index.php
+++ Views/Posts/index.php
@@ -2,7 +2,7 @@
 <div class="row col-md-12 centered">
     <table class="table table-striped custab">
         <thead>
-        <a href="/proyecto-MVC/comments/create/" class="btn btn-primary btn-xs pull-right"><b>+</b>Añadir Post</a>
+      
         <tr>
             <th>ID</th>
             <th>Comentario</th>
@@ -17,7 +17,7 @@
             echo "<td>" . $post['id'] . "</td>";
             echo "<td>" . $post['title'] . "</td>";
            
-            echo "<td class='text-center'><a class='btn btn-info btn-xs' href='/proyecto-MVC/comments/edit/" . $comment["id"] . "' ><span class='glyphicon glyphicon-edit'></span> Editar</a> <a href='/proyecto-MVC/comments/delete/" . $comment["id"] . "' class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove'></span> Borrar</a></td>";
+            echo "<td class='text-center'><a class='btn btn-info btn-xs' href=" . WEBROOT . 'posts/edit/'.$post["id"] . " ><span class='glyphicon glyphicon-edit'></span> Editar</a> <a href=" . WEBROOT . 'posts/delete/'.$post["id"] . " class='btn btn-danger btn-xs'><span class='glyphicon glyphicon-remove'></span> Borrar</a></td>";
             echo "</tr>";
         }
         ?>
